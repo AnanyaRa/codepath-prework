f6cf8f16d85c92f34cbbb0e4f32c25ce0b2e0f63
Ananya Rajagopalan <ananya.rajagopalan@gmail.com>
2022-02-02 16:08:53 -0500

    Added prework files to repository

---
+++ ananya codepath ios prework/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ananya codepath ios prework
+//
+//  Created by Ananya Rajagopalan on 2/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var TextLabel: UILabel!
+    @IBAction func ButtonClicked(_ sender: Any) {
+        print("hello")
+        TextLabel.textColor = UIColor.orange
+    }
+    
+}
+
